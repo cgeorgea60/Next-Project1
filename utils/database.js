ac90ccf0eb4322d9e1d986f5ca44a5ad21abed84
cgeorgea60 <64281418+cgeorgea60@users.noreply.github.com>
2023-07-30 02:28:05 +0100

    First commit

---
+++ utils/database.js
@@ -0,0 +1,27 @@
+import mongoose from "mongoose";
+
+
+let isConnected = false; // track the db connection.
+
+
+export const connectToDB = async () => {
+    mongoose.set('strictQuery', true);
+
+    if (isConnected) {
+        console.log('MongoDB is already connected');
+    }
+
+    try {
+        await mongoose.connect(process.env.MONGODB_URI, {
+            dbName: 'share_prompt',
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+        })
+
+        isConnected = true;
+        console.log('MongoDB is connected')
+    } catch (error) {
+        console.log(error);
+    }
+}
+
